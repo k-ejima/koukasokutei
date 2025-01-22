b2f66af5a08dfab24ef965cd96a56e8082a97ea3
k-ejima <fko2447004@stu.o-hara.ac.jp>
2025-01-22 15:26:24 +0900

    second commit

--- Ejinews/admin.py
+++ Ejinews/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import AddNews
+from .models import AddNews,Comment
 
 admin.site.register(AddNews)
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('user', 'news', 'content', 'posted_at')  # 管理画面に表示するフィールドを指定
+    search_fields = ('user__username', 'content')  # 検索フィールドを指定
+
+admin.site.register(Comment, CommentAdmin)  # Commentモデルを登録
